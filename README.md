2b8aea4470c2b03145503bdc0d9ebfd24059436c
ikuhana <popp.marko@gmail.com>
2017-02-18 22:08:57 +0200

    Basic data logging

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # sense-hat_datalogger
+
+Simple sqlite datalogger for RasperryPi Sense-Hat written in rust for
+personal learning purposes.  
+Uses SenseHat Rust library and Rusqlite
+
+ToDo:
+Add timestamp
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+extern crate sensehat;
+extern crate rusqlite;
+
+use sensehat::*;
+use std::{thread, time};
+use rusqlite::Connection;
+
+struct SenseData {
+    humidity: f64,
+    temperature: f64,
+    p_temperature: f64, 
+    h_temperature: f64, 
+    pressure: f64
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let wait = time::Duration::from_millis(1000);
+    let mut sense = SenseHat::new().unwrap();
+
+    sense.init();
+    
+    //~ let conn = Connection::open_in_memory().unwrap();
+    let conn = Connection::open("SenseData.sql").unwrap();
+    
+    
+
+    conn.execute("CREATE TABLE sense_data (
+                  humidity  FLOAT,
+                  temperature   FLOAT,
+                  p_temperature FLOAT,
+                  h_temperature FLOAT,
+                  pressure  FLOAT
+                  )", &[]).unwrap();
+    
+    loop {
+        let res = SenseData {
+            humidity: sense.get_humidity() as f64,
+            temperature: sense.get_temperature() as f64,
+            p_temperature: sense.get_temperature_from_pressure() as f64,
+            h_temperature: sense.get_temperature_from_humidity() as f64,
+            pressure: sense.get_pressure() as f64
+        };
+        conn.execute("INSERT INTO sense_data (
+                humidity, 
+                temperature, 
+                p_temperature, 
+                h_temperature, 
+                pressure)
+            VALUES (?1, ?2, ?3, ?4, ?5)",
+                 &[&res.humidity, &res.temperature, &res.p_temperature, 
+                 &res.h_temperature, &res.pressure]).unwrap();
+        thread::sleep(wait);
+    }
 }
